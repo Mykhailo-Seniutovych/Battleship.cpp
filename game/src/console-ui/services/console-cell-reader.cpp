56e4ff29c323a1a8a99796f136a6cefcd7bbf48f
Mykhailo Seniutovych <Mykhailo.Seniutovych@Eleks.com>
2021-09-28 16:34:02 +0300

    fix validation for entering new target

--- game/src/console-ui/services/console-cell-reader.cpp
+++ game/src/console-ui/services/console-cell-reader.cpp
@@ -11,25 +11,34 @@ using namespace map_utils;
 
 Cell ConsoleCellReader::readCell() const
 {
-    cout << "Enter coordinates to shoot at (e.g. \"A 10\")" << endl;
-    string newTarget;
-    getline(cin, newTarget);
+	cout << "Enter coordinates to shoot at (e.g. \"A 10\")" << endl;
+	string newTarget;
+	getline(cin, newTarget);
 
-    auto rowLetter = newTarget[0];
-    auto rowNumberResult = convertRowLetterToRowNumber(rowLetter);
-    if (rowNumberResult < 1)
-    {
-        cerr << INVALID_INPUT_ERROR_MSG << endl;
-        return readCell();
-    }
+	auto rowLetter = newTarget[0];
+	auto rowNumberResult = convertRowLetterToRowNumber(rowLetter);
+	if (rowNumberResult < 1)
+	{
+		cerr << INVALID_INPUT_ERROR_MSG << endl;
+		return readCell();
+	}
 
-    uint8_t rowNum = rowNumberResult - 1;
-    auto colNumberFromUserInput = stoi(newTarget.substr(newTarget.find(" ") + 1));
-    if (colNumberFromUserInput < 1 || colNumberFromUserInput > Constants::MAP_SIZE)
-    {
-        cerr << INVALID_INPUT_ERROR_MSG << endl;
-        return readCell();
-    }
-    auto colNum = colNumberFromUserInput - 1;
-    return Cell(rowNum, colNum);
+	try
+	{
+		uint8_t rowNum = rowNumberResult - 1;
+		auto colNumberFromUserInput = stoi(newTarget.substr(newTarget.find(" ") + 1));
+		if (colNumberFromUserInput < 1 || colNumberFromUserInput > Constants::MAP_SIZE)
+		{
+			cerr << INVALID_INPUT_ERROR_MSG << endl;
+			return readCell();
+		}
+		auto colNum = colNumberFromUserInput - 1;
+		return Cell(rowNum, colNum);
+	}
+	catch (exception ex)
+	{
+		cerr << INVALID_INPUT_ERROR_MSG << endl;
+		return readCell();
+	}
+
 }
